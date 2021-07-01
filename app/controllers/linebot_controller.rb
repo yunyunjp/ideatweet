adc967b685270ec321b73c95faf37b73014a2235
yunyunjp <78527774+yunyunjp@users.noreply.github.com>
2021-07-01 23:22:13 +0900

    キーワードを１つLINEで入力できる
    １つキーワードを渡した場合、そのキーワードとランダムなキーワードを返してくれる機能

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,3 +1,4 @@
+
 class LinebotController < ApplicationController
   require 'line/bot'
   # callbackアクションのCSRFトークン認証を無効
@@ -14,18 +15,34 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          seed1 = select_word
-          seed2 = select_word
-          while seed1 == seed2
+          # 正規表現で「〜』をパターンマッチしてkeywordへ格納
+          keyword = event.message['text'].match(/.*「(.+)」.*/)
+          # マッチングしたときのみ入力されたキーワードを使用
+          if  keyword.present?
             seed2 = select_word
+            message = [{
+              type: 'text',
+              text: "そのキーワードなかなかいいね〜"
+            },{
+              type: 'text',
+              # keyword[1]：「」内の文言
+              text: "#{keyword[1]} × #{seed2} !!"
+            }]
+          # マッチングしなかった場合は元々の仕様と同じようにキーワードを2つ選択して返す
+          else
+            seed1 = select_word
+            seed2 = select_word
+            while seed1 == seed2
+              seed2 = select_word
+            end
+            message = [{
+              type: 'text',
+              text: "キーワード何にしようかな〜〜"
+            },{
+              type: 'text',
+              text: "#{seed1} × #{seed2} !!"
+            }]
           end
-          message = [{
-            type: 'text',
-            text: "キーワード何にしようかな〜〜"
-          },{
-            type: 'text',
-            text: "#{seed1} × #{seed2} !!"
-          }]
           client.reply_message(event['replyToken'], message)
         end
       end
